18b8b358cb906123089d98284f758446bbad6cf5
Mark Bell <mark00bell@googlemail.com>
2018-10-21 04:12:13 +0100

    Added compression.

--- periodic/settings.py
+++ periodic/settings.py
@@ -117,6 +117,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
