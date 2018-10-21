f65ef23371a88643ed50a7dbbd5b2fd621512dbe
Mark Bell <mark00bell@googlemail.com>
2018-10-21 04:36:00 +0100

    Moved to another folder.

--- periodic/settings.py
+++ periodic/settings.py
@@ -127,5 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static/']
 
 django_heroku.settings(locals())
