d966861aa9fe76e4a09e82a454f576f484e16615
Mark Bell <mark00bell@googlemail.com>
2018-10-21 03:00:55 +0100

    Trying to get more info.

--- periodic/settings.py
+++ periodic/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', True))
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
 
